753bc225e025b02d39b541acd6395ac5e1d0dea1
Bruno Cruz <brunocruz012013@gmail.com>
2022-01-17 20:21:07 -0300

    Adicionando Print da tela

--- prog-flutter/exercicio_mycard/lib/main.dart
+++ prog-flutter/exercicio_mycard/lib/main.dart
@@ -6,10 +6,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Exercício MyCard',
